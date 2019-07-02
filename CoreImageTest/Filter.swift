386a87638bc75ab13a70ac5f4f57fb45fc883e6a
khhk10 <kohe.ikeda@gmail.com>
2019-07-02 19:47:13 +0900

    CIColorControls filter in Filter.swift
    brightness / contrast / saturation

---
+++ CoreImageTest/Filter.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+extension ViewController {
+    // 明度/コントラスト/彩度
+    func colorControls(_ input: CIImage, intensity: Double, type: ColorControl) -> CIImage? {
+        let filter = CIFilter(name: "CIColorControls")
+        filter?.setValue(input, forKey: kCIInputImageKey)
+        
+        switch type {
+        case .brightness:
+            filter?.setValue(intensity, forKey: kCIInputBrightnessKey)
+        case .contrast:
+            filter?.setValue(intensity, forKey: kCIInputContrastKey)
+        default:
+            filter?.setValue(intensity, forKey: kCIInputSaturationKey)
+        }
+        return filter?.outputImage
+    }
+    
+    enum ColorControl {
+        case brightness
+        case contrast
+        case saturation
+    }
+    
+    // セピアフィルタ
+    func sepiaFilter(_ input: CIImage, intensity: Double) -> CIImage? {
+        let sepiaFilter = CIFilter(name: "CISepiaTone")
+        sepiaFilter?.setValue(input, forKey: kCIInputImageKey)
+        sepiaFilter?.setValue(intensity, forKey: kCIInputIntensityKey)
+        
+        return sepiaFilter?.outputImage
+    }
+    
+    // クランプフィルタ
+    func clampFilter(_ input: CIImage, inputMin: CIVector, inputMax: CIVector) -> CIImage? {
+        let clampFilter = CIFilter(name: "CIColorClamp")
+        clampFilter?.setValue(input, forKey: kCIInputImageKey)
+        clampFilter?.setValue(inputMin, forKey: "inputMinComponents")
+        clampFilter?.setValue(inputMax, forKey: "inputMaxComponents")
+        
+        return clampFilter?.outputImage
+    }
+}
--- CoreImageTest/ViewController.swift
+++ CoreImageTest/ViewController.swift
@@ -6,6 +6,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var beforeImage: UIImageView!
     @IBOutlet weak var afterImage: UIImageView!
+    var origiImage: CIImage!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -14,44 +15,32 @@ class ViewController: UIViewController {
         
         // let url = URL(fileURLWithPath: "\(Bundle.main.bundlePath)/Assets.xcassets/photo1.jpg")
         
-        // オリジナル画像
+        // 処理前の画像
         guard let filePath = Bundle.main.path(forResource: "photo4", ofType: "jpg") else {
             // ファイルが存在しなかった
             return
         }
         let url = URL(fileURLWithPath: filePath)
-        let origiImage = CIImage(contentsOf: url)!
-        // self.beforeImage.contentMode = .scaleAspectFill
-        // self.beforeImage.clipsToBounds = true
+        origiImage = CIImage(contentsOf: url)!
         self.beforeImage.image = UIImage(ciImage: origiImage)
         
         // セピアフィルタ
         // let sepiaImage = sepiaFilter(origiImage, intensity: 0.9)
         
         // クランプフィルタ
-        let clampImage = clampFilter(origiImage, inputMin: CIVector(x: 0, y: 0, z: 0, w: 0), inputMax: CIVector(x: 1.0, y: 1.0, z: 0.3, w: 0.8))
+        // let clampImage = clampFilter(origiImage, inputMin: CIVector(x: 0, y: 0, z: 0, w: 0), inputMax: CIVector(x: 1.0, y: 1.0, z: 0.3, w: 0.8))
         
         // 処理後の画像
-        self.afterImage.image = UIImage(ciImage: clampImage!)
+        self.afterImage.image = UIImage(ciImage: origiImage!)
     }
     
-    // セピアフィルタ
-    func sepiaFilter(_ input: CIImage, intensity: Double) -> CIImage? {
-        let sepiaFilter = CIFilter(name: "CISepiaTone")
-        sepiaFilter?.setValue(input, forKey: kCIInputImageKey)
-        sepiaFilter?.setValue(intensity, forKey: kCIInputIntensityKey)
-        
-        return sepiaFilter?.outputImage
-    }
-    
-    // クランプフィルタ
-    func clampFilter(_ input: CIImage, inputMin: CIVector, inputMax: CIVector) -> CIImage? {
-        let clampFilter = CIFilter(name: "CIColorClamp")
-        clampFilter?.setValue(input, forKey: kCIInputImageKey)
-        clampFilter?.setValue(inputMin, forKey: "inputMinComponents")
-        clampFilter?.setValue(inputMax, forKey: "inputMaxComponents")
-        
-        return clampFilter?.outputImage
+    // スライダー
+    @IBAction func changeSlider(_ sender: UISlider) {
+        DispatchQueue.main.async {
+            // コントラスト
+            let contrastImage = self.colorControls(self.origiImage, intensity: Double(sender.value), type: .contrast)
+            self.afterImage.image = UIImage(ciImage: contrastImage!)
+        }
     }
 }
 
